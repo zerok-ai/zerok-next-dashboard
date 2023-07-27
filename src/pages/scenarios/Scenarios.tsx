33eddb773656d4b438934770d1cfe3ab32f7d7b5
Santhosh J <ssjdexucyche@gmail.com>
2023-07-27 16:17:19 +0530

    added table for scenarios page

--- src/pages/scenarios/Scenarios.tsx
+++ src/pages/scenarios/Scenarios.tsx
@@ -1,12 +1,19 @@
+import { Checkbox } from "@mui/material";
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import PageLayout from "components/layouts/PageLayout";
 import PrivateRoute from "components/PrivateRoute";
+import TableX from "components/themeX/TableX";
 import { useFetch } from "hooks/useFetch";
-import { nanoid } from "nanoid";
 import Head from "next/head";
+// import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { clusterSelector } from "redux/cluster";
 import { useSelector } from "redux/store";
-import { renderScenarioString } from "utils/scenarios/functions";
+import { DEFAULT_COL_WIDTH } from "utils/constants";
 import { type ScenarioDetail } from "utils/scenarios/types";
 
 import styles from "./Scenarios.module.scss";
@@ -15,12 +22,46 @@ const Scenarios = () => {
   const { data: scenarios, fetchData: fetchScenarios } = useFetch<
     ScenarioDetail[]
   >("scenarios", null);
+  // const router = useRouter();
+  // const range = router.query.range ?? DEFAULT_TIME_RANGE;
   const { selectedCluster, renderTrigger } = useSelector(clusterSelector);
   useEffect(() => {
     if (selectedCluster) {
+      // fetchScenarios(
+      //   LIST_SCENARIOS_ENDPOINT.replace(
+      //     "{cluster_id}",
+      //     selectedCluster
+      //   ).replace("{range}", range as string)
+      // );
       fetchScenarios("/scenarios.json");
     }
   }, [selectedCluster, renderTrigger]);
+
+  const helper = createColumnHelper<ScenarioDetail>();
+
+  const columns = [
+    helper.accessor("scenario_id", {
+      header: "ID",
+      size: DEFAULT_COL_WIDTH / 4,
+    }),
+    helper.accessor("scenario_title", {
+      header: "Issue",
+    }),
+    helper.accessor("scenario_type", {
+      header: "Type",
+    }),
+    helper.accessor("enabled", {
+      cell: (info) => {
+        return <Checkbox checked={info.row.original.enabled} />;
+      },
+    }),
+  ];
+
+  const table = useReactTable({
+    data: scenarios ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   if (!scenarios) {
     return null;
@@ -28,9 +69,11 @@ const Scenarios = () => {
 
   return (
     <div className={styles.container}>
-      {scenarios.map((scenario) => {
+      {/* {scenarios.map((scenario) => {
         return <p key={nanoid()}>{renderScenarioString(scenario.workloads)}</p>;
-      })}
+      })} */}
+      <h3 className={"page-title"}>Issues</h3>
+      <TableX table={table} data={scenarios} />
     </div>
   );
 };
---
+++ src/utils/scenarios/endpoints.ts
@@ -0,0 +1,1 @@
+export const LIST_SCENARIOS_ENDPOINT = `v1/o/cluster/{cluster_id}/scenario?last_sync_ts=0`;
