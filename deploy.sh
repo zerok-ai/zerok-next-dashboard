7115d9e8d771e707ac5d7ba89c7c22b38644e3de
Santhosh J <ssjdexucyche@gmail.com>
2023-08-17 15:09:16 +0530

    removed v1 scripts, added zerokdev deployment

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ yarn export
 docker build . -t us-west1-docker.pkg.dev/zerok-dev/zerok-dashboard/zerok-next-dashboard:dev
 docker push us-west1-docker.pkg.dev/zerok-dev/zerok-dashboard/zerok-next-dashboard:dev
 
-# kubectl apply -k ./k8s
+kubectl apply -k ./k8s
 
 ./killpod.sh
 
