7767137271d09ba9674713689d2683755e6304de
Santhosh J <ssjdexucyche@gmail.com>
2023-08-17 15:09:16 +0530

    removed v1 scripts, added zerokdev deployment

--- src/pages/logout/Logout.tsx
+++ src/pages/logout/Logout.tsx
@@ -1,6 +1,8 @@
 import AuthFormCard from "components/forms/AuthFormCard";
+import AuthLayout from "components/layouts/AuthLayout";
+import Head from "next/head";
 import Link from "next/link";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { logoutUser } from "redux/authSlice";
 import { useDispatch } from "redux/store";
 
@@ -11,12 +13,22 @@ const Logout = () => {
   }, []);
   return (
     <AuthFormCard title="Logout">
+      {/* Head / Meta tags */}
+      <Fragment>
+        <Head>
+          <title>ZeroK Dashboard | Logout</title>
+        </Head>
+      </Fragment>
       <h6>
         You&apos;re logged out. Click <Link href="/login">here</Link> to log in
         again.
       </h6>
     </AuthFormCard>
   );
+};
+
+Logout.getLayout = function getLayout(page: React.ReactNode) {
+  return <AuthLayout>{page}</AuthLayout>;
 };
 
 export default Logout;
