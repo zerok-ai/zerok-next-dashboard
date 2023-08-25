002e6d5a200250f8facf0c0e55e80c4fdb834640
Santhosh J <ssjdexucyche@gmail.com>
2023-08-25 18:13:38 +0530

    added pagination

--- src/pages/zk-gpt/list/ZkGptList.tsx
+++ src/pages/zk-gpt/list/ZkGptList.tsx
@@ -8,10 +8,12 @@ import CustomSkeleton from "components/CustomSkeleton";
 import PageHeader from "components/helpers/PageHeader";
 import PageLayout from "components/layouts/PageLayout";
 import PrivateRoute from "components/PrivateRoute";
+import PaginationX from "components/themeX/PaginationX";
 import TableX from "components/themeX/TableX";
 import { useFetch } from "hooks/useFetch";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { clusterSelector } from "redux/cluster";
 import { useSelector } from "redux/store";
@@ -23,6 +25,10 @@ import { type IssueDetail } from "utils/types";
 import { type GptReplyWithScore } from "../ZkGptPrompter.utils";
 import styles from "./ZkGptList.module.scss";
 
+const PAGE_SIZE = 10;
+
+export const CLUSTER = "d980180f-7314-451f-8cd0-0cd4516f3a00";
+
 const ZkGptList = () => {
   const { data, fetchData, loading } =
     useFetch<GptReplyWithScore[]>("UserInferences");
@@ -30,10 +36,12 @@ const ZkGptList = () => {
     useFetch<IssueDetail[]>("issues");
   const { selectedCluster, renderTrigger } = useSelector(clusterSelector);
   const helper = createColumnHelper<GptReplyWithScore>();
+  const router = useRouter();
+  const page = router.query.page ?? "1";
   useEffect(() => {
     if (selectedCluster) {
       fetchIssues(
-        LIST_ISSUES_ENDPOINT.replace("{cluster_id}", selectedCluster)
+        LIST_ISSUES_ENDPOINT.replace("{cluster_id}", CLUSTER)
           .replace("{range}", DEFAULT_TIME_RANGE)
           .replace("{limit}", "100")
           .replace("{offset}", "0")
@@ -44,14 +52,15 @@ const ZkGptList = () => {
   const [selectedIssue, setSelectedIssue] = useState<string | null>(null);
   useEffect(() => {
     if (selectedIssue) {
+      const params = `?limit=${PAGE_SIZE}&offset=${Number(page) - 1}`;
       fetchData(
         GPT_LIST_INFERENCES_ENDPOINT.replace(
           "{issue_id}",
           selectedIssue
-        ).replace("{cluster_id}", selectedCluster as string)
+        ).replace("{cluster_id}", CLUSTER as string) + params
       );
     }
-  }, [selectedIssue, renderTrigger]);
+  }, [selectedIssue, renderTrigger, router]);
   const columns = [
     helper.accessor("query", {
       header: "Query",
@@ -111,6 +120,7 @@ const ZkGptList = () => {
         extras={extras}
         showRefresh={true}
       />
+
       <div className={styles.container}>
         {loading ? (
           <CustomSkeleton len={8} />
@@ -118,6 +128,7 @@ const ZkGptList = () => {
           <TableX table={table} data={data ?? []} />
         )}
       </div>
+      <PaginationX itemsPerPage={PAGE_SIZE} totalItems={100} />
     </div>
   );
 };
