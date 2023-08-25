16125f8b0d3ee202c44b406745d6a45532e563bf
Santhosh J <ssjdexucyche@gmail.com>
2023-08-25 13:46:50 +0530

    added gpt endpoint

--- src/utils/gpt/endpoints.ts
+++ src/utils/gpt/endpoints.ts
@@ -3,3 +3,5 @@ export const GPT_SCENARIO_ENDPOINT = `/v1/c/{cluster_id}/gpt/scenario/{scenario_
 export const GPT_ISSUE_ENDPOINT = `/v1/c/{cluster_id}/gpt/issue/{issue_id}`;
 
 export const GPT_INCIDENT_ENDPOINT = `/v1/c/{cluster_id}/gpt/issue/{issue_id}/incident/{incident_id}`;
+
+export const GPT_PROMPT_OBSERVABILITY_ENDPOINT = `/v1/c/gpt/issue/observation`;
