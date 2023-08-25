11cbb0fbab9f9ad2e8158d9ffa334dda05ae1f1f
Santhosh J <ssjdexucyche@gmail.com>
2023-08-25 16:24:29 +0530

    added post request

--- src/utils/gpt/endpoints.ts
+++ src/utils/gpt/endpoints.ts
@@ -4,4 +4,4 @@ export const GPT_ISSUE_ENDPOINT = `/v1/c/{cluster_id}/gpt/issue/{issue_id}`;
 
 export const GPT_INCIDENT_ENDPOINT = `/v1/c/{cluster_id}/gpt/issue/{issue_id}/incident/{incident_id}`;
 
-export const GPT_PROMPT_OBSERVABILITY_ENDPOINT = `/v1/c/gpt/issue/observation`;
+export const GPT_PROMPT_OBSERVABILITY_ENDPOINT = `/v1/c/{cluster_id}/gpt/issue/observation`;
