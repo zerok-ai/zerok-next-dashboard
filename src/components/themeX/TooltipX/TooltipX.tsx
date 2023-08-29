fce1e4a12c6ce0e55c733e7c46bd00bc30a60059
Santhosh J <ssjdexucyche@gmail.com>
2023-08-29 16:34:58 +0530

    fixes for build error

--- src/components/themeX/TooltipX/TooltipX.tsx
+++ src/components/themeX/TooltipX/TooltipX.tsx
@@ -1,13 +1,10 @@
 import { Tooltip } from "@mui/material";
-import { type ReactNode } from "react";
-
-import styles from "./TooltipX.module.scss";
 
 interface TooltipXProps {
   placement?: "top" | "bottom" | "left" | "right";
   arrow?: boolean;
   title: string;
-  children: ReactNode;
+  children: React.ReactElement;
 }
 
 const TooltipX = ({
