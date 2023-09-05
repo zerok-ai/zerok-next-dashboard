8e5d734e0c8c9a7f4bd11a9a785eeceac4462ed2
Santhosh J <ssjdexucyche@gmail.com>
2023-09-05 18:43:58 +0530

    updated tree width

--- src/components/TraceTree/TraceTree.utils.tsx
+++ src/components/TraceTree/TraceTree.utils.tsx
@@ -136,7 +136,7 @@ export const getWidthByLevel = (
   leaf: boolean = false,
   expand = true
 ) => {
-  const defaultWidth = expand ? 600 : 350;
+  const defaultWidth = expand ? 650 : 430;
   const width = defaultWidth - level * 9;
   return leaf ? `${width + 10}px` : `${width}px`;
 };
