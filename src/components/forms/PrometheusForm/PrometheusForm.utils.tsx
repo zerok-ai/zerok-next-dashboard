914ca83808108fb188cbad7ae717c7da3da1018e
Santhosh J <ssjdexucyche@gmail.com>
2023-09-07 14:19:55 +0530

    added host to table for prom list

--- src/components/forms/PrometheusForm/PrometheusForm.utils.tsx
+++ src/components/forms/PrometheusForm/PrometheusForm.utils.tsx
@@ -1,4 +1,9 @@
+import { FormHelperText, OutlinedInput } from "@mui/material";
+import cx from "classnames";
+import { type FieldErrors, type UseFormRegister } from "react-hook-form";
 import { z } from "zod";
+
+import styles from "./PrometheusForm.module.scss";
 
 export const PROMETHEUS_LEVELS = ["cluster", "org"] as const;
 
@@ -7,7 +12,10 @@ export const PromFormSchema = z.object({
   url: z
     .string()
     .nonempty("URL cannot be empty")
-    .url("Please enter a valid URL"),
+    .regex(
+      /^(https?:\/\/)?[a-zA-Z0-9.-]+(:\d+)?(\/[\w/.-]*)?$/,
+      "Please enter a valid URL"
+    ),
   username: z.string(),
   password: z.string(),
   level: z.enum(["cluster", "org"]),
@@ -28,3 +36,33 @@ export const PROM_LEVEL_OPTIONS: Array<{
     label: "Organization",
   },
 ];
+
+export const FormItem = ({
+  id,
+  label,
+  register,
+  helperText,
+  errors,
+}: {
+  id: keyof PromFormSchemaType;
+  label: string;
+  register: UseFormRegister<PromFormSchemaType>;
+  helperText: string;
+  errors: FieldErrors<PromFormSchemaType>;
+}) => {
+  return (
+    <div className={styles["form-item-container"]}>
+      <div className={styles["form-group"]}>
+        <label htmlFor={id}>{label}</label>
+        <OutlinedInput {...register(id)} id={id} name={id} />
+      </div>
+      <FormHelperText
+        className={cx(
+          errors[id] ? styles["error-text"] : styles["helper-text"]
+        )}
+      >
+        {errors[id] ? errors[id]!.message : helperText}
+      </FormHelperText>
+    </div>
+  );
+};
