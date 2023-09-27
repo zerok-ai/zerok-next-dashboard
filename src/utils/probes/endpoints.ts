3b0f1fab3da4a3af7154cd2d54ce04f54bfbd066
Santhosh J <ssjdexucyche@gmail.com>
2023-09-27 16:10:49 +0530

    removed cluster id from endpoint of attributes

--- src/utils/probes/endpoints.ts
+++ src/utils/probes/endpoints.ts
@@ -1,1 +1,1 @@
-export const PROBE_ATTRIBUTES_ENDPOINT = `/v1/u/cluster/{cluster_id}/attribute?protocol={protocol}`;
+export const PROBE_ATTRIBUTES_ENDPOINT = `/v1/u/cluster/attribute?protocol={protocol}`;
