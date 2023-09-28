932db2d6d1f7384871faad59d2afed2c52b53e17
Santhosh J <ssjdexucyche@gmail.com>
2023-09-28 14:45:07 +0530

    added new design for trace table;

--- src/components/traces/TraceTable/TraceTable.utils.tsx
+++ src/components/traces/TraceTable/TraceTable.utils.tsx
@@ -1,6 +1,7 @@
 import { createColumnHelper } from "@tanstack/react-table";
+import cx from "classnames";
 import ChipX from "components/themeX/ChipX";
-import { DEFAULT_COL_WIDTH, type HTTP_METHODS } from "utils/constants";
+import { DEFAULT_COL_WIDTH } from "utils/constants";
 import { formatDuration, getFormattedTime } from "utils/dateHelpers";
 import { convertNanoToMilliSeconds, trimString } from "utils/functions";
 import { type TraceMetadataDetail } from "utils/issues/types";
@@ -8,31 +9,29 @@ import { type TableSortOptions } from "utils/tables/types";
 
 import styles from "./TraceTable.module.scss";
 
-export const HttpActionRender = ({
-  action,
-}: {
-  action: (typeof HTTP_METHODS)[number];
-}) => {
-  switch (action) {
-    case "DELETE":
-      return <span className={styles["action-delete"]}>DELETE</span>;
-    default:
-      return <span className={styles["action-default"]}>{action}</span>;
-  }
-};
-
 const helper = createColumnHelper<TraceMetadataDetail>();
 
-export const getTraceColumns = (chatTrace: string | null) => {
+export const getTraceColumns = ({
+  chatTrace,
+  currentTrace,
+}: {
+  chatTrace: string;
+  currentTrace: string;
+}) => {
   return [
     helper.accessor("entry_path", {
       header: "REQUEST ENTRY POINT",
-      size: DEFAULT_COL_WIDTH * 2,
+      size: DEFAULT_COL_WIDTH * 1.5,
       enableSorting: false,
       cell: (info) => {
         return (
           <div className={styles["entry-point-container"]}>
-            <span className={styles["entry-point"]}>
+            <span
+              className={cx(
+                styles["entry-point"],
+                currentTrace === info.row.original.incident_id && styles.active
+              )}
+            >
               {trimString(info.getValue(), 30)}{" "}
             </span>
             {chatTrace === info.row.original.incident_id && (
@@ -62,7 +61,7 @@ export const getTraceColumns = (chatTrace: string | null) => {
       // sortingFn: (a: string, b: string) => {
       //   return dayjs(a).unix() - dayjs(b).unix();
       // },
-      size: DEFAULT_COL_WIDTH * 1.2,
+      size: DEFAULT_COL_WIDTH * 1.5,
       cell: (info) => {
         return <span>{getFormattedTime(info.getValue())}</span>;
       },
