ef6dfa40d5ebd8c334600a85e7d359c3b20699ca
Santhosh J <ssjdexucyche@gmail.com>
2023-10-19 13:29:54 +0530

    updated deployment docker script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 set -x #echo on
 
+# Check if the number of parameters is not equal to 2
+if [ $# -ne 2 ]; then
+    echo "Please provide exactly 2 parameters. Cluster and tag."
+    exit 1
+fi
+
 yarn
 yarn build
 
-docker build . -t us-west1-docker.pkg.dev/zerok-dev/zerok-dashboard/zerok-next-dashboard:0.1.0
-docker push us-west1-docker.pkg.dev/zerok-dev/zerok-dashboard/zerok-next-dashboard:0.1.0
+echo "Deploying to $1 cluster"
+
+docker build . -t us-west1-docker.pkg.dev/zerok-dev/zerok-dashboard/$1/zerok-next-dashboard:$2
+docker push us-west1-docker.pkg.dev/zerok-dev/zerok-dashboard/$1/zerok-next-dashboard:$2
 
 # kubectl apply -k ./k8s
 
-./killpod.sh
+# ./killpod.sh
 
