4836b88ee1cf112b9dc1d5a7ac9ce8d5bc8b65bb
Santhosh J <ssjdexucyche@gmail.com>
2023-10-26 15:58:37 +0530

    added otel and ebpf tables

--- src/components/OTelListTable/OTelListTable.tsx
+++ src/components/OTelListTable/OTelListTable.tsx
@@ -1,10 +1,25 @@
 import AddNewBtn from "components/helpers/AddNewBtn";
 import PageHeader from "components/helpers/PageHeader";
+import TableX from "components/themeX/TableX";
+import dayjs from "dayjs";
 import { Fragment } from "react";
+import { useSelector } from "redux/store";
+import { type OtelIntegrationListType } from "utils/integrations/types";
+
+import { OTEL_COLUMNS } from "./OTelListTable.utils";
 
 // import styles from "./OTelListTable.module.scss";
 
 const OTelListTable = () => {
+  const { clusters } = useSelector((state) => state.cluster);
+  const columns = OTEL_COLUMNS;
+  const data: OtelIntegrationListType[] = clusters.map((cluster) => ({
+    name: cluster.name,
+    created_at: dayjs().toString(),
+    created_by: "thalapathy vijay",
+    updated_at: dayjs().toString(),
+    integration_status: Math.random() * 100,
+  }));
   return (
     <Fragment>
       <PageHeader
@@ -23,6 +38,7 @@ const OTelListTable = () => {
           />,
         ]}
       />
+      <TableX data={data} columns={columns} />
     </Fragment>
   );
 };
---
+++ src/components/OTelListTable/OTelListTable.utils.tsx
@@ -0,0 +1,38 @@
+import { LinearProgress } from "@mui/material";
+import { createColumnHelper } from "@tanstack/react-table";
+import { DEFAULT_COL_WIDTH } from "utils/constants";
+import { getFormattedTime } from "utils/dateHelpers";
+import { type OtelIntegrationListType } from "utils/integrations/types";
+
+const helper = createColumnHelper<OtelIntegrationListType>();
+export const OTEL_COLUMNS = [
+  helper.accessor("name", {
+    header: "Cluster name",
+    size: DEFAULT_COL_WIDTH * 2,
+  }),
+  helper.accessor("created_at", {
+    header: "Created at",
+    size: DEFAULT_COL_WIDTH,
+    cell: (info) => {
+      return getFormattedTime(info.getValue());
+    },
+  }),
+  helper.accessor("created_by", {
+    header: "Created by",
+    size: DEFAULT_COL_WIDTH,
+  }),
+  helper.accessor("updated_at", {
+    header: "Last updated",
+    size: DEFAULT_COL_WIDTH,
+    cell: (info) => {
+      return getFormattedTime(info.getValue());
+    },
+  }),
+  helper.accessor("integration_status", {
+    header: "Completion status",
+    size: DEFAULT_COL_WIDTH / 2,
+    cell: (info) => {
+      return <LinearProgress value={info.getValue()} variant="determinate" />;
+    },
+  }),
+];
