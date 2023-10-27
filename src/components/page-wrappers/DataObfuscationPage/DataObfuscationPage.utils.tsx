bcd4df076cdbed99678d230d1f365978ccd3d250
Santhosh J <ssjdexucyche@gmail.com>
2023-10-27 16:25:19 +0530

    added default obfs rules

--- src/components/page-wrappers/DataObfuscationPage/DataObfuscationPage.utils.tsx
+++ src/components/page-wrappers/DataObfuscationPage/DataObfuscationPage.utils.tsx
@@ -1,9 +1,14 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import TableActions from "components/helpers/TableActions";
 import { DEFAULT_COL_WIDTH } from "utils/constants";
-import { type ObfuscationRuleType } from "utils/data/types";
+import {
+  type DefaultRegexRuleType,
+  type ObfuscationRuleType,
+} from "utils/data/types";
 import { getFormattedTime } from "utils/dateHelpers";
 import { type TableActionPropType } from "utils/tables/types";
+
+import styles from "./DataObfuscationPage.module.scss";
 
 export const getObfuscationColumns = ({
   actions,
@@ -83,3 +88,30 @@ export const data: ObfuscationRuleType[] = [
     },
   },
 ];
+
+const rhelper = createColumnHelper<DefaultRegexRuleType>();
+
+export const getRuleColumns = ({
+  onRuleClick,
+}: {
+  onRuleClick: (row: DefaultRegexRuleType) => void;
+}) => {
+  return [
+    rhelper.accessor("name", {
+      header: "Name",
+      size: DEFAULT_COL_WIDTH * 2,
+      cell: (info) => {
+        return (
+          <span
+            onClick={() => {
+              onRuleClick(info.row.original);
+            }}
+            className={styles["rule-name"]}
+          >
+            {info.getValue()}
+          </span>
+        );
+      },
+    }),
+  ];
+};
--- src/utils/data/constants.ts
+++ src/utils/data/constants.ts
@@ -5,8 +5,12 @@ export const DATA_OBFUSCATION_TABS: Array<{
   value: ObfuscationTabType;
 }> = [
   {
-    label: "Obfuscation rules",
-    value: "rules",
+    label: "Default Obfuscation Rules",
+    value: "default",
+  },
+  {
+    label: "Custom Obfuscation Rules",
+    value: "custom",
   },
   {
     label: "Whitelist",
