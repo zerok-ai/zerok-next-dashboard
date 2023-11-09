0bbb769cee8e1e451baca45fa411c46c2c1cbf3e
Santhosh J <ssjdexucyche@gmail.com>
2023-11-09 10:04:11 +0530

    added fetchuser

--- src/utils/auth/endpoints.ts
+++ src/utils/auth/endpoints.ts
@@ -1,5 +1,7 @@
 export const LOGIN_ENDPOINT = `/v1/p/auth/login`;
 
+export const USER_DETAILS_ENDPOINT = `/v1/u/user/details`;
+
 export const FORGOT_PASSWORD_ENDPOINT = `/v1/p/user/password/recover/{email}`;
 
 export const LOGOUT_ENDPOINT = `/v1/u/auth/logout`;
