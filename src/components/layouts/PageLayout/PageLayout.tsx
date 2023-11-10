d186f8989eb80722b2a012ca856314b097e34f1e
Santhosh J <ssjdexucyche@gmail.com>
2023-11-10 13:58:51 +0530

    added id to output step

--- src/components/layouts/PageLayout/PageLayout.tsx
+++ src/components/layouts/PageLayout/PageLayout.tsx
@@ -18,7 +18,7 @@ const PageLayout = ({
   showClusterModal = false,
 }: PageLayoutProps) => {
   const { isDrawerMinimized } = useSelector(drawerSelector);
-  console.log(process.env.FLAG_ENV)
+  console.log({ isDrawerMinimized }, process.env.FLAG_ID, "HEEY");
   return (
     <div className={styles.container}>
       <aside className={styles["drawer-container"]}>
