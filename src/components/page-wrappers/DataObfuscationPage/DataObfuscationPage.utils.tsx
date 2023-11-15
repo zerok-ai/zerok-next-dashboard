cce1a7d37e38ea0a413eb91c13eaf84768ed5493
Santhosh J <ssjdexucyche@gmail.com>
2023-11-15 16:49:47 +0530

    added integration for obfuscation API

--- src/components/page-wrappers/DataObfuscationPage/DataObfuscationPage.utils.tsx
+++ src/components/page-wrappers/DataObfuscationPage/DataObfuscationPage.utils.tsx
@@ -1,12 +1,14 @@
-import { Switch } from "@mui/material";
 import { createColumnHelper } from "@tanstack/react-table";
+import cx from "classnames";
+import EnableDisableTableAction from "components/EnableDisableTableAction";
 import TableActions from "components/helpers/TableActions";
+import TableTimeCell from "components/TableTimeCell";
+import ChipX from "components/themeX/ChipX";
 import { DEFAULT_COL_WIDTH } from "utils/constants";
 import {
   type DefaultRegexRuleType,
   type ObfuscationRuleType,
 } from "utils/data/types";
-import { getFormattedTime } from "utils/dateHelpers";
 import { type TableActionItem } from "utils/tables/types";
 
 import styles from "./DataObfuscationPage.module.scss";
@@ -14,106 +16,81 @@ import styles from "./DataObfuscationPage.module.scss";
 export const getObfuscationColumns = ({
   onEdit,
   onUpdate,
+  selectedRule,
 }: {
   onEdit: (row: ObfuscationRuleType) => void;
   onUpdate: (row: ObfuscationRuleType) => void;
+  selectedRule: ObfuscationRuleType | null;
 }) => {
   const helper = createColumnHelper<ObfuscationRuleType>();
   const columns = [
     helper.accessor("name", {
       header: "Name",
-      size: DEFAULT_COL_WIDTH,
+      size: DEFAULT_COL_WIDTH * 3,
+      cell: (info) => {
+        return (
+          <div
+            className={cx(
+              styles["name-container"],
+              !info.row.original.enabled && styles.disabled
+            )}
+          >
+            <span
+              onClick={() => {
+                onEdit(info.row.original);
+              }}
+              className={styles["rule-name"]}
+            >
+              {info.getValue()}
+            </span>
+            {!info.row.original.enabled && (
+              <ChipX label="Disabled" color="disabled" />
+            )}
+          </div>
+        );
+      },
     }),
     helper.accessor("analyzer.pattern", {
       header: "Pattern",
       size: DEFAULT_COL_WIDTH * 4,
     }),
-    helper.accessor("analyzer.type", {
+    helper.accessor("created_by", {
       header: "Created by",
-      size: DEFAULT_COL_WIDTH,
+      size: DEFAULT_COL_WIDTH * 1.2,
     }),
     helper.accessor("updated_at", {
       header: "Last updated",
-      size: DEFAULT_COL_WIDTH / 1.2,
-      cell: (info) => {
-        return getFormattedTime(info.getValue());
-      },
+      size: DEFAULT_COL_WIDTH * 1.2,
+      cell: (info) => <TableTimeCell time={info.getValue()} />,
     }),
     helper.display({
       id: "actions",
-      size: DEFAULT_COL_WIDTH / 2,
+      size: DEFAULT_COL_WIDTH / 3,
       cell: (info) => {
         const row = info.row.original;
         const { enabled } = row;
         const actions: TableActionItem[] = [
+          {
+            element: <EnableDisableTableAction isEnabled={enabled} />,
+            onClick: () => {
+              onUpdate(row);
+            },
+          },
           {
             element: <span>Edit</span>,
             onClick: () => {
               onEdit(row);
             },
           },
-          {
-            element: (
-              <div className={styles["action-item"]}>
-                {enabled ? "Disable" : "Enable"}
-                <Switch
-                  defaultChecked={enabled}
-                  className={styles.switch}
-                  size="medium"
-                  onChange={() => {
-                    onUpdate(row);
-                  }}
-                />
-              </div>
-            ),
-            onClick: () => {
-              onUpdate(row);
-            },
-          },
         ];
-        return <TableActions list={actions} loading={false} />;
+        return (
+          <TableActions list={actions} loading={selectedRule?.id === row.id} />
+        );
       },
     }),
   ];
   return columns;
 };
-
-export const data: ObfuscationRuleType[] = [
-  {
-    name: "test",
-    analyzer: {
-      pattern: "test",
-      type: "regex",
-    },
-    enabled: true,
-    created_by: "test",
-    updated_at: "test",
-    created_at: "test",
-    anonymizer: {
-      operator: "replace",
-      params: {
-        new_value: "test",
-      },
-    },
-  },
-  {
-    name: "test 2",
-    analyzer: {
-      pattern: "test",
-      type: "regex",
-    },
-    enabled: true,
-    created_by: "test 2",
-    updated_at: "test",
-    created_at: "test",
-    anonymizer: {
-      operator: "replace",
-      params: {
-        new_value: "test 2",
-      },
-    },
-  },
-];
 
 const rhelper = createColumnHelper<DefaultRegexRuleType>();
 
---
+++ src/utils/data/endpoint.ts
@@ -0,0 +1,5 @@
+export const OBFUSCATION_RULE_CREATION_ENDPOINT = `/v1/u/org/obfuscation/rule`;
+
+export const LIST_OBFUSCATION_RULE_ENDPOINT = `v1/u/org/obfuscation/rule/list`;
+
+export const UPDATE_OBFUSCATION_RULE_ENDPOINT = `v1/u/org/obfuscation/{obfuscation_id}/rule`;
